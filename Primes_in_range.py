bbf7cf15f8c638b345b1ec19f4da761abcba3799
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-06-28 21:43:47 +0530

    import math def fun(n):     if(n==1):         return 0     if(n==2):         return 1     for i in range(2,int(math.sqrt(n))+1):         if(n%i==0):             return 0     else:         return 1; n=int(input()) m=int(input()) c=0 for i in range(n,m+1):     if(fun(i)):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,10 +1,12 @@
 import math
 def fun(n):
-    if(n<2):
-        return 0;
+    if(n==1):
+        return 0
+    if(n==2):
+        return 1
     for i in range(2,int(math.sqrt(n))+1):
         if(n%i==0):
-            return 0;
+            return 0
     else:
         return 1;
 n=int(input())
