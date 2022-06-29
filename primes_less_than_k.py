99fdd31a2ed34f1cbaa6756f04a1bb555e949594
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-06-29 19:46:00 +0530

    import math def fun(n):     if(n==1):         return 0     if(n==2):         return 1     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return 0     return 1 n=int(input()) a=list(map(int,input().split()))[:n] k=int(input()) s=0 for i in a:     if(k>=i):         if(fun(i)):             s+=1 print(s)

---
+++ primes_less_than_k.py
@@ -0,0 +1,19 @@
+import math
+def fun(n):
+    if(n==1):
+        return 0
+    if(n==2):
+        return 1
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return 0
+    return 1
+n=int(input())
+a=list(map(int,input().split()))[:n]
+k=int(input())
+s=0
+for i in a:
+    if(k>=i):
+        if(fun(i)):
+            s+=1
+print(s)
