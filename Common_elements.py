7a4635ad62243ef8b4049ab3aa55105a5fec2fd5
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-10-12 19:24:22 +0530

    a,b=map(int,input().split()) m=list(map(int,input().split()))[:a] n=list(map(int,input().split()))[:b] c=0 p=[] q=[] for i in m:     if i not in  p:         p.append(i) for i in n:     if i not in q:         q.append(i)
    for i in p:
        if i in q:
            print(i,end=' ')

---
+++ Common_elements.py
@@ -0,0 +1,16 @@
+a,b=map(int,input().split())
+m=list(map(int,input().split()))[:a]
+n=list(map(int,input().split()))[:b]
+c=0
+p=[]
+q=[]
+for i in m:
+    if i not in  p:
+        p.append(i)
+for i in n:
+    if i not in q:
+        q.append(i)
+
+for i in p:
+    if i in q:
+        print(i,end=' ')
