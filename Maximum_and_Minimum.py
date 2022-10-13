84d6db8f84b02c7e5d711231c066e2bac11d82bc
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-10-13 19:46:05 +0530

    n=int(input()) l=list(map(int,input().split()))[:n] s=set(l) a=[] for i in s:     if l.count(i)==i:         a.append(i) if(len(a)==0):     print("-1") else:     print(min(a),max(a))

---
+++ Maximum_and_Minimum.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))[:n]
+s=set(l)
+a=[]
+for i in s:
+    if l.count(i)==i:
+        a.append(i)
+if(len(a)==0):
+    print("-1")
+else:
+    print(min(a),max(a))
