ff149db2486cb42f4ee246a40caa792a9cd9230a
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-12-15 21:25:32 +0530

    s=input() l=s.split() t='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' for i in range(0,len(l)):     if(i%2==0):         t+=''.join(reversed(l[i]))         if(i!=len(l)-1):             t+=" "     else:         t+=''.join(l[i])         if(i!=len(l)-1):             t+=" " print(t)

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,7 +1,13 @@
 s=input()
-k=s.split()
-for i in range(len(k)):
+l=s.split()
+t='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'
+for i in range(0,len(l)):
     if(i%2==0):
-        print(k[i][::-1],end=' ')
+        t+=''.join(reversed(l[i]))
+        if(i!=len(l)-1):
+            t+=" "
     else:
-        print(k[i],end=' ')
+        t+=''.join(l[i])
+        if(i!=len(l)-1):
+            t+=" "
+print(t)
