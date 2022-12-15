43ede656c7a1a6f1cbe3ebad744fb134688ee60d
VASAVI <97425195+21A91A05G9@users.noreply.github.com>
2022-12-15 21:04:48 +0530

    n=int(input()) a=[] l=list(map(int,input().split())) for i in range(0,len(l),2):     while(l[i+1]):         a.append(l[i])         l[i+1]-=1 print(*a)

--- Generate_array_I.py
+++ Generate_array_I.py
@@ -1,9 +1,8 @@
-a=int(input())
-l=list(map(int,input().split()))[:a]
-k=[]
+n=int(input())
+a=[]
+l=list(map(int,input().split()))
 for i in range(0,len(l),2):
-    x=l[i+1]
-    while(x):
-        k.append(l[i])
-        x-=1
-print(*k)
+    while(l[i+1]):
+        a.append(l[i])
+        l[i+1]-=1
+print(*a)
